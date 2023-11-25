3e574b9d111a696c617427f287819fe3b5a3df5d
iyusuf <punkgts40@gmail.com>
2023-11-25 16:36:38 +0100

    add more setup scripts

--- server_setup_scripts.sh
+++ server_setup_scripts.sh
@@ -2,7 +2,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get -y install python3-pip
-pip3 uninstall Fabric
 sudo apt-get install -y libffi-dev
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y build-essential
@@ -14,7 +13,7 @@ pip3 install setuptools==40.1.0
 pip3 install cryptography==2.8
 pip3 install bcrypt==3.1.7
 pip3 install PyNaCl==1.3.0
-pip3 install Fabric3==1.14.post1
+pip3 install Fabric3==1.14.post1 # use sudo pip3 if 'fab not found'
 sudo apt install -y mysql-server
 sudo apt-get install -y python3-dev
 sudo apt-get install -y libmysqlclient-dev
