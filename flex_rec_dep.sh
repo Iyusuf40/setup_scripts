cea7ab14cc2ab73d27eea604f493f0efd38bfb59
iyusuf <punkgts40@gmail.com>
2024-10-05 17:35:45 +0100

    update flex_records dep

--- flex_rec_dep.sh
+++ flex_rec_dep.sh
@@ -4,6 +4,8 @@ sudo apt-get -y upgrade
 sudo apt-get install python3-pip -y
 sudo pip3 install Flask
 sudo pip3 install flask-cors
+sudo pip3 install redis
+sudo pip3 install flask-sock
 sudo apt-get install curl -y
 sudo apt-get install wget -y
 curl -sL https://deb.nodesource.com/setup_19.x | sudo -E bash -
